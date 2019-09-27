656e3323e3d852be27d09a649808b69cf3e51ebd
AndyOmondi17 <andyomondi17@gmail.com>
2019-09-27 14:23:22 +0300

    first commit

---
+++ my-first-website/images/60 Interesting Monkey Facts _ Fun Facts about Monkeys_files/Eg5f-wY0NO3.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["HRQu2"]); }
+
+__d("cxMargin",["cx"],(function(a,b,c,d,e,f,g){"use strict";function a(a){throw new Error("cxMargin: Unexpected margin transformation.")}e.exports=a}),null);
---
+++ project/js/scripts.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+$('h1').click(function(){
+    alert("This is a heading tag");
+    alert("I told you this is a header");
+  });
+
+  $('p').click(function(){
+    alert("This is a paragraph")
+  });
+
+  $('img').click(function(){
+    alert("This is an image");
+
+  });
+});
